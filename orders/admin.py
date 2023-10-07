c673a68e9c496bd822ea49b3d6ff42f1117594e5
Charymyrat Garryyev <kcharymyrat@gmail.com>
2023-10-07 10:06:14 +0500

    models for orders and products apps are done

--- orders/admin.py
+++ orders/admin.py
@@ -2,6 +2,41 @@ from django.contrib import admin
 
 from .models import CartItem, Cart, Order
 
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ["id", "total_price", "is_ordered", "get_cart_items"]
+    list_display_links = ["get_cart_items"]
+    readonly_fields = ["total_price", "is_ordered", "get_cart_items"]
+
+    def get_cart_items(self, instance):
+        return [cart_item for cart_item in instance.cart_items.all()]
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "cart",
+        "status",
+        "delivery_time",
+        "payment_option",
+        "customer_name",
+        "phone",
+        "shipping_address",
+        "total_order_price",
+        "all_cart_items",
+    ]
+    list_filter = ["status", "phone", "payment_option"]
+    readonly_fields = ("total_order_price",)
+    list_display_links = ["cart"]
+    list_editable = ["status"]
+
+    def get_cart_items(self, instance):
+        return [cart for cart in instance.carts.all()]
+
+    def get_readonly_fields(self, request, obj=None):
+        return self.readonly_fields + ("all_cart_items",)
+
+
 admin.site.register(CartItem)
-admin.site.register(Cart)
-admin.site.register(Order)
+admin.site.register(Cart, CartAdmin)
+admin.site.register(Order, OrderAdmin)
--- orders/models.py
+++ orders/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.urls import reverse
 from django.utils.html import mark_safe
 from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
 
 from django.conf.global_settings import LANGUAGE_CODE
@@ -13,12 +14,20 @@ from products.models import Product
 
 User = settings.AUTH_USER_MODEL
 
-# TODO: Orgonize the Cart and Order so the Cart is no longer available or has a status changed to ordered
+phone_regex = RegexValidator(
+    regex=r"^\d{6}$",
+    message=_(
+        "Phone number must be entered in the format: '12345678'. Exactly 8 digits."
+    ),
+)
 
 
 class Cart(models.Model):
     products = models.ManyToManyField(
-        Product, verbose_name=_("products"), through="CartItem"
+        Product,
+        verbose_name=_("products"),
+        through="CartItem",
+        related_name="carts",
     )
     total_price = models.DecimalField(
         _("cart price"), max_digits=10, decimal_places=2, default=0.00
@@ -33,18 +42,30 @@ class Cart(models.Model):
         ordering = ["date_added"]
 
     def update_total_price(self):
-        self.total_price = sum(item.sub_total for item in self.cartitem_set.all())
+        self.total_price = sum(item.sub_total for item in self.cart_items.all())
+        self.save()
+
+    def update_is_order(self):
+        self.is_ordered = True
         self.save()
 
     def __str__(self):
-        return str(self.id)
+        return f"Cart {self.id}: {self.total_price} manats"
 
 
 class CartItem(models.Model):
     product = models.ForeignKey(
-        Product, verbose_name=_("product"), on_delete=models.CASCADE
+        Product,
+        verbose_name=_("product"),
+        on_delete=models.CASCADE,
+        related_name="cart_items",
     )
-    cart = models.ForeignKey(Cart, verbose_name=_("cart"), on_delete=models.CASCADE)
+    cart = models.ForeignKey(
+        Cart,
+        verbose_name=_("cart"),
+        on_delete=models.CASCADE,
+        related_name="cart_items",
+    )
     quantity = models.PositiveIntegerField(_("quantity"), default=1)
 
     @property
@@ -60,22 +81,54 @@ class CartItem(models.Model):
         self.cart.update_total_price()  # Update the cart's total price after saving.
 
     def __str__(self):
-        return f"{self.quantity} of {self.product.title}"
+        return f"{self.quantity}-{self.product.title} = {self.sub_total}"
 
 
 class Order(models.Model):
     ORDER_STATUSES = (
         (_("Created"), _("Created")),
         (_("Ordered"), _("Ordered")),
+        (_("Shipped"), _("Shipped")),
+        (_("Completed"), _("Completed")),
+        (_("Cancelled"), _("Cancelled")),
+    )
+    PAYMENT_CHOICES = (
+        (_("Cash"), _("Cash")),
+        (_("Card Terminal"), _("Card Terminal")),
     )
 
     user = models.ForeignKey(
-        User, verbose_name=_("user"), on_delete=models.CASCADE, null=True, blank=True
+        User,
+        verbose_name=_("user"),
+        on_delete=models.CASCADE,
+        related_name="order",
+        null=True,
+        blank=True,
     )
-    cart = models.ForeignKey(Cart, verbose_name=_("cart"), on_delete=models.CASCADE)
+    cart = models.ForeignKey(
+        Cart,
+        verbose_name=_("cart"),
+        on_delete=models.CASCADE,
+        related_name="order",
+    )
     status = models.CharField(
         _("status"), max_length=120, choices=ORDER_STATUSES, default="Created"
     )
+    payment_option = models.CharField(
+        _("payment option"), max_length=120, choices=PAYMENT_CHOICES, default="Cash"
+    )
+    customer_name = models.CharField(_("customer name"), max_length=255)
+    phone = models.CharField(
+        _("phone number"),
+        validators=[phone_regex],
+        max_length=6,
+        help_text=_("8 digits"),
+    )
+    delivery_time = models.DateTimeField(
+        _("delivery date and time"), auto_now=False, auto_now_add=False
+    )
+    shipping_address = models.TextField(_("shipping address"))
+    email = models.EmailField(_("email"), max_length=254, null=True, blank=True)
     total_order_price = models.DecimalField(
         _("total order price"), default=0.00, max_digits=100, decimal_places=2
     )
@@ -86,9 +139,13 @@ class Order(models.Model):
         verbose_name_plural = _("orders")
         ordering = ["-timestamp"]
 
+    @property
+    def all_cart_items(self):
+        return [cart_item for cart_item in self.cart.cart_items.all()]
+
     def save(self) -> None:
         self.total_order_price = self.cart.total_price
-        self.cart.is_ordered = True
+        self.cart.update_is_order()
         return super().save()
 
     def __str__(self):
