1c19e66adf6984d69cb22fc14fa482303497b3e2
Charymyrat Garryyev <kcharymyrat@gmail.com>
2023-10-27 12:21:28 +0500

    HTML and CSS if successfully done for cart.html

--- dj_project/settings/_base.py
+++ dj_project/settings/_base.py
@@ -121,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "tk"
+LANGUAGE_CODE = "en"
 
 LANGUAGES = [
     ("tk", _("Turkmen")),
