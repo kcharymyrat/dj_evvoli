2205082e34fa66f34c86f6e0c1eae4207c282529
Charymyrat Garryyev <kcharymyrat@gmail.com>
2023-11-11 09:36:49 +0500

    Added humanize to installed at and implemented it in template to make line breaks in TextField, also have done changes to navbar template

--- dj_project/settings/_base.py
+++ dj_project/settings/_base.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Additional Django app, apart form default provided ones
+    "django.contrib.humanize",
     # Third party
     "imagekit",
     "django_htmx",
