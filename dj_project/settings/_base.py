93260d0a898815d3ad370a03eabd5bc55f25a5c9
Charymyrat Garryyev <kcharymyrat@gmail.com>
2024-01-12 15:16:08 +0500

    Stable before Docker implementation

--- dj_project/settings/_base.py
+++ dj_project/settings/_base.py
@@ -32,7 +32,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["192.168.1.14", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.1.15", "localhost", "127.0.0.1"]
 
 
 # Application definition
