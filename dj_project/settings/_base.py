52831a71e53d8372924328a3602a3f8362b70cdf
Charymyrat Garryyev <kcharymyrat@gmail.com>
2024-01-18 18:42:44 +0500

    Everything seems ok

--- dj_project/settings/_base.py
+++ dj_project/settings/_base.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 load_dotenv()  # loads the configs from .env
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
